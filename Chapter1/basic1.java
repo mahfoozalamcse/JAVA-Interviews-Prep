405e66605c3e2ed36f3102b28da1315276264f5e
mahfoozalamcse <amahfooz1007@gmail.com>
2026-04-05 04:19:22 +0530

    Practice Mock test 1 urlshortener

--- Chapter1/basic1.java
+++ Chapter1/basic1.java
@@ -2,6 +2,6 @@ package Chapter1;
 
 public class basic1 {
     public static void main(String[] args) {
-        
+       
     }
 }
---
+++ Chapter1/mockTestURLShort.java
@@ -0,0 +1,49 @@
+package Chapter1;
+
+import java.util.*;
+
+public class mockTestURLShort {
+
+    Map<String, String> shortToLong = new HashMap<>();
+    Map<String, String> longToShort = new HashMap<>();
+    String baseUrl = "http://mahfooz.com/";
+
+    int counter = 1;
+    public String encode(String longUrl){
+       if (longToShort.containsKey(longUrl)) {
+           return baseUrl + longToShort.get(longUrl);
+       }
+
+       String shortKey = getBase62(counter++);
+       shortToLong.put(shortKey, longUrl);
+       longToShort.put(longUrl, shortKey);
+
+       return baseUrl + shortKey;
+    }
+
+    public String decode(String shortUrl){
+       String key = shortUrl.replace(baseUrl, "");
+       return shortToLong.get(key);
+    }
+
+    public String getBase62(int counter){
+        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder sb = new StringBuilder();
+        while (counter > 0) {
+
+            sb.append(chars.charAt(counter % 62));
+            counter /= 62;
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        mockTestURLShort result = new mockTestURLShort();
+        String result1 = result.encode("https://www.google.com/search?q=java");
+        String result2 = result.decode("http://mahfooz.com/b");
+        System.out.println(" Answer encoded string :"+ result1);
+        System.out.println(" Answer decoded url " + result2);
+
+    }
+}
